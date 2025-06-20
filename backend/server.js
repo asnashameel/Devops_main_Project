229bf8fba6b583c87d196c9368f2877faf6a33af
asnashameel <155845311+asnashameel@users.noreply.github.com>
2025-06-20 15:59:34 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,9 @@ const pool = new Pool({
   database: process.env.DB_NAME || 'cloudops_demo',
   password: process.env.DB_PASSWORD || 'password',
   port: process.env.DB_PORT || 5432,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 // Middleware
